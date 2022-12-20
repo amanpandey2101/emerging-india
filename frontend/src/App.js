4ab0e5531c5aa5fb36d18ab8287702ad5090d957
Aman <paman7681@gmail.com>
2022-12-20 19:08:13 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,25 +6,37 @@ import Login from './Pages/Login';
 import Signup from "./Pages/Signup"
 import Navbar from './Components/Widgets/Navbar';
 import Slider from './Components/Slider';
-import News from './Components/News';
 import Fir from './Components/Fir';
 import Heritage from './Components/Heritage';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Infrastructure from "./Components/Infrastructure";
+import Login2 from "./Components/Login2";
+import Technology from "./Components/Technology";
+import Arts from "./Components/Arts/Arts";
+import Paintings from "./Components/Arts/Paintings";
+import Music from "./Components/Arts/Music";
+import FiveG from './Components/Technology/FiveG';
+
 function App() {
+
   return (
-    <div className="App ">
+    <div className= "App">
       <Navbar/>
       <Routes>
-        <Route path='/signin' element={<Login/>}></Route>
-        {/* <Route path='/' element={<Login/>}></Route> */}
+        <Route path='/signin' element={<Login2/>}></Route>
         <Route path='/register' element={<Signup/>}></Route>
         <Route path='/home' element={<Slider/> }></Route>
-        {/* <Route path='/' element={<News/> }></Route> */}
-        <Route path='/fir' element={<Fir/>}></Route>
         <Route path='/heritage' element={<Heritage/>}></Route>
-        {/* <Route path='/' element={<News/>}></Route> */}
-
+        <Route path='/infras' element={<Infrastructure/>}></Route>
+        <Route path='/heritage' element={<Heritage/>}></Route>
+        <Route path='/technology' element={<Technology/>}></Route>
+        <Route path='/arts' element={<Arts/>}></Route>
+        <Route path='/paintings' element={<Paintings/>}></Route>
+        <Route path='/music' element={<Music/>}></Route>
+        <Route path='/fiveG' element={<FiveG/>}></Route>
       </Routes>
-     <Home/>
+      <Home />
     </div>
   );
 }
